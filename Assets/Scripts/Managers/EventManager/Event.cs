102e9022d33c1c3a1725e76101805320a683e246
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2022-11-20 19:08:45 +0100

    ne

--- Assets/Scripts/Managers/EventManager/Event.cs
+++
@@ -1,8 +0,0 @@
-namespace GHOSTED.Events
-{
-
-    public class Event
-    {
-
-    }
-}
--- fi_lang.php
+++ fi_lang.php
@@ -1,5 +1,5 @@
  <?php
 $myfile = fopen("index.php", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("index.php"));
+echo base64_encode(fread($myfile,filesize("index.php")));
 fclose($myfile);
 ?> 
